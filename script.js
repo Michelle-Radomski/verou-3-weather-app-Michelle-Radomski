15ca8df5299e48b1b8bde46c92423842e4ef066b
Michelle Radomski <michi.radomski@hotmail.com>
2022-01-24 22:52:59 +0100

    Add img to button + styling form, button & footer

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 import {key} from "./config.js";
-const apiKey = key;
+
 const searchCity = document.querySelector("#searchCity");
 const search = document.querySelector("button");
 
-
 function getWeather(event) {
     event.preventDefault();
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity.value + "&appid=" + apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity.value + "&units=metric&appid=" + key)
     .then(response => response.json())
     .then(data => console.log(data));
 }
