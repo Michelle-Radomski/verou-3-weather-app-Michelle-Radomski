fb2df9d7f9125fbe6f63d1141bda203ec787078f
Michelle Radomski <michi.radomski@hotmail.com>
2022-01-24 15:26:51 +0100

    Some styling, basic structure body + function to get data

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 import {key} from "./config.js";
 const apiKey = key;
 const searchCity = document.querySelector("#searchCity");
+const search = document.querySelector("button");
 
-fetch("api.openweathermap.org/data/2.5/weather?q=" + searchCity.innerHTML + "&appid=" + apiKey)
-.then(response => response.json())
-.then(data => console.log(data));
+
+function getWeather(event) {
+    event.preventDefault();
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity.value + "&appid=" + apiKey)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+search.addEventListener("click", getWeather);
